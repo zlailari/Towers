7c42b3c298bc1c16add676be176cff0f5aaa8682
andy <andysalerno@gmail.com>
2016-02-25 17:25:17 -0600

    add grid_world.py, which I forgot to do in the last commit from my desktop

---
+++ backend/grid_world.py
@@ -0,0 +1,14 @@
+class GridWorld:
+
+    def __init__(self, width, height, spawnpoint, endpoint):
+        self.width = width
+        self.height = height
+        self.spawnpoint = spawnpoint
+        self.endpoint = endpoint
+
+        # this grid is a 2d array of bools, where grid[3][4] means
+        # x,y position (4,3) is either obstructed (True) or traversable (False)
+        self.grid = [[False for x in self.width] for y in self.height]
+
+    def is_blocked(self, x, y):
+        return self.grid[y][x]
