137cb64b49669af4ea9dda3d55ae5f07ae81009b
andy <andysalerno@gmail.com>
2016-02-25 19:19:21 -0600

    whoops, forgot to add the amin server driver

---
+++ backend/server_driver.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This is the top-level driver to run the server.
+This only exists so that the scope of this program begins
+somewhere above all the child directories."""
+
+import engine.tower_server as server
+
+server.main()
