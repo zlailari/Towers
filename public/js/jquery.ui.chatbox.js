719c7a02b221d21b3530cfd67fba9b02ee512566
erikwijmans <ewijmans2@gmail.com>
2016-03-01 00:20:58 -0600

    Expandable Chatbox

--- public/js/jquery.ui.chatbox.js
+++ public/js/jquery.ui.chatbox.js
@@ -74,184 +74,195 @@
             self._scrollToBottom();
           });
         },
+        isVisible: function () {
+          return this.elem.uiChatboxContent.is(":visible");
+        },
         toggleBox: function() {
-          this.elem.uiChatbox.toggle();
+          this.elem.uiChatboxContent.toggle();
+          if (this.isVisible()) {
+            this.elem.uiChatboxInputBox.focus();
+            $( ".ui-icon.ui-icon-minusthick" ).text("Minimize");
+          } else {
+            $( ".ui-icon.ui-icon-minusthick" ).text("Expand");
+          }
         },
         _scrollToBottom: function() {
           var box = this.elem.uiChatboxLog;
           box.scrollTop(box.get(0).scrollHeight);
-          }
         }
-      },
-      toggleContent: function(event) {
-        this.uiChatboxContent.toggle();
-        if (this.uiChatboxContent.is(":visible")) {
-          this.uiChatboxInputBox.focus();
-        }
-      },
-      widget: function() {
-        return this.uiChatbox
-      },
-      _create: function() {
-        var self = this,
-        options = self.options,
-        title = options.title || "No Title",
-        // chatbox
-        uiChatbox = (self.uiChatbox = $('<div></div>'))
-        .appendTo(document.body)
-        .addClass('ui-widget ' +
-          'ui-corner-top ' +
-          'ui-chatbox'
-          )
-        .attr('outline', 0)
-        .focusin(function() {
-          // ui-state-highlight is not really helpful here
-          //self.uiChatbox.removeClass('ui-state-highlight');
-          self.uiChatboxTitlebar.addClass('ui-state-focus');
-        })
-        .focusout(function() {
-          self.uiChatboxTitlebar.removeClass('ui-state-focus');
-        }),
-        // titlebar
-        uiChatboxTitlebar = (self.uiChatboxTitlebar = $('<div></div>'))
-        .addClass('ui-widget-header ' +
-          'ui-corner-top ' +
-          'ui-chatbox-titlebar ' +
-          'ui-dialog-header' // take advantage of dialog header style
-            )
-        .click(function(event) {
-          self.toggleContent(event);
-        })
-        .appendTo(uiChatbox),
-        uiChatboxTitle = (self.uiChatboxTitle = $('<span></span>'))
-        .html(title)
-        .appendTo(uiChatboxTitlebar),
-        uiChatboxTitlebarClose = (self.uiChatboxTitlebarClose = $('<a href="#"></a>'))
-        .addClass('ui-corner-all ' +
-          'ui-chatbox-icon '
+      }
+    },
+    toggleContent: function(event) {
+      this.uiChatboxContent.toggle();
+      if (this.uiChatboxContent.is(":visible")) {
+        this.uiChatboxInputBox.focus();
+        $( ".ui-icon.ui-icon-minusthick" ).text("Minimize");
+      } else {
+        $( ".ui-icon.ui-icon-minusthick" ).text("Expand");
+      }
+    },
+    widget: function() {
+      return this.uiChatbox
+    },
+    _create: function() {
+      var self = this,
+      options = self.options,
+      title = options.title || "No Title",
+      // chatbox
+      uiChatbox = (self.uiChatbox = $('<div></div>'))
+      .appendTo(document.body)
+      .addClass('ui-widget ' +
+        'ui-corner-top ' +
+        'ui-chatbox'
         )
-        .attr('role', 'button')
-        .hover(function() { uiChatboxTitlebarClose.addClass('ui-state-hover'); },
-         function() { uiChatboxTitlebarClose.removeClass('ui-state-hover'); })
-        .click(function(event) {
-          uiChatbox.hide();
-          self.options.boxClosed(self.options.id);
-          return false;
-        })
-        .appendTo(uiChatboxTitlebar),
-        uiChatboxTitlebarCloseText = $('<span></span>')
-        .addClass('ui-icon ' +
-          'ui-icon-closethick')
-        .text('close')
-        .appendTo(uiChatboxTitlebarClose),
-        uiChatboxTitlebarMinimize = (self.uiChatboxTitlebarMinimize = $('<a href="#"></a>'))
-        .addClass('ui-corner-all ' +
-          'ui-chatbox-icon'
+      .attr('outline', 0)
+      .focusin(function() {
+        // ui-state-highlight is not really helpful here
+        //self.uiChatbox.removeClass('ui-state-highlight');
+        self.uiChatboxTitlebar.addClass('ui-state-focus');
+      })
+      .focusout(function() {
+        self.uiChatboxTitlebar.removeClass('ui-state-focus');
+      }),
+      // titlebar
+      uiChatboxTitlebar = (self.uiChatboxTitlebar = $('<div></div>'))
+      .addClass('ui-widget-header ' +
+        'ui-corner-top ' +
+        'ui-chatbox-titlebar ' +
+        'ui-dialog-header' // take advantage of dialog header style
           )
-        .attr('role', 'button')
-        .hover(function() { uiChatboxTitlebarMinimize.addClass('ui-state-hover'); },
-         function() { uiChatboxTitlebarMinimize.removeClass('ui-state-hover'); })
-        .click(function(event) {
-          self.toggleContent(event);
-          return false;
-        })
-        .appendTo(uiChatboxTitlebar),
-        uiChatboxTitlebarMinimizeText = $('<span></span>')
-        .addClass('ui-icon ' +
-          'ui-icon-minusthick')
-        .text('minimize')
-        .appendTo(uiChatboxTitlebarMinimize),
-        // content
-        uiChatboxContent = (self.uiChatboxContent = $('<div></div>'))
-        .addClass('ui-widget-content ' +
-          'ui-chatbox-content '
-          )
-        .appendTo(uiChatbox),
-        uiChatboxLog = (self.uiChatboxLog = self.element)
-        .addClass('ui-widget-content ' +
-          'ui-chatbox-log'
-          )
-        .appendTo(uiChatboxContent),
-        uiChatboxInput = (self.uiChatboxInput = $('<div></div>'))
-        .addClass('ui-widget-content ' +
-          'ui-chatbox-input'
-          )
-        .click(function(event) {
-                // anything?
-              })
-        .appendTo(uiChatboxContent),
-        uiChatboxInputBox = (self.uiChatboxInputBox = $('<textarea></textarea>'))
-        .addClass('ui-widget-content ' +
-          'ui-chatbox-input-box ' +
-          'ui-corner-all'
-          )
-        .appendTo(uiChatboxInput)
-        .keydown(function(event) {
-          if (event.keyCode && event.keyCode == $.ui.keyCode.ENTER) {
-            msg = $.trim($(this).val());
-            if (msg.length > 0) {
-              self.options.messageSent(self.options.id, self.options.user, msg);
-            }
-            $(this).val('');
-            return false;
+      .click(function(event) {
+        self.toggleContent(event);
+      })
+      .appendTo(uiChatbox),
+      uiChatboxTitle = (self.uiChatboxTitle = $('<span></span>'))
+      .html(title)
+      /*.appendTo(uiChatboxTitlebar),
+      uiChatboxTitlebarClose = (self.uiChatboxTitlebarClose = $('<a href="#"></a>'))
+      .addClass('ui-corner-all ' +
+        'ui-chatbox-icon '
+      )
+      .attr('role', 'button')
+      .hover(function() { uiChatboxTitlebarClose.addClass('ui-state-hover'); },
+       function() { uiChatboxTitlebarClose.removeClass('ui-state-hover'); })
+      .click(function(event) {
+        uiChatbox.hide();
+        self.options.boxClosed(self.options.id);
+        return false;
+      })
+      .appendTo(uiChatboxTitlebar),
+      uiChatboxTitlebarCloseText = $('<span></span>')
+      .addClass('ui-icon ' +
+        'ui-icon-closethick')
+      .text('close')*/
+      .appendTo(uiChatboxTitlebar),
+      uiChatboxTitlebarMinimize = (self.uiChatboxTitlebarMinimize = $('<a href="#"></a>'))
+      .addClass('ui-corner-all ' +
+        'ui-chatbox-icon'
+        )
+      .attr('role', 'button')
+      .hover(function() { uiChatboxTitlebarMinimize.addClass('ui-state-hover'); },
+       function() { uiChatboxTitlebarMinimize.removeClass('ui-state-hover'); })
+      .click(function(event) {
+        self.toggleContent(event);
+        return false;
+      })
+      .appendTo(uiChatboxTitlebar),
+      uiChatboxTitlebarMinimizeText = $('<span></span>')
+      .addClass('ui-icon ' +
+        'ui-icon-minusthick')
+      .text('Expand')
+      .appendTo(uiChatboxTitlebarMinimize),
+      // content
+      uiChatboxContent = (self.uiChatboxContent = $('<div></div>'))
+      .addClass('ui-widget-content ' +
+        'ui-chatbox-content '
+        )
+      .appendTo(uiChatbox),
+      uiChatboxLog = (self.uiChatboxLog = self.element)
+      .addClass('ui-widget-content ' +
+        'ui-chatbox-log'
+        )
+      .appendTo(uiChatboxContent),
+      uiChatboxInput = (self.uiChatboxInput = $('<div></div>'))
+      .addClass('ui-widget-content ' +
+        'ui-chatbox-input'
+        )
+      .click(function(event) {
+              // anything?
+            })
+      .appendTo(uiChatboxContent),
+      uiChatboxInputBox = (self.uiChatboxInputBox = $('<textarea></textarea>'))
+      .addClass('ui-widget-content ' +
+        'ui-chatbox-input-box ' +
+        'ui-corner-all'
+        )
+      .appendTo(uiChatboxInput)
+      .keydown(function(event) {
+        if (event.keyCode && event.keyCode == $.ui.keyCode.ENTER) {
+          msg = $.trim($(this).val());
+          if (msg.length > 0) {
+            self.options.messageSent(self.options.id, self.options.user, msg);
           }
-        })
-        .focusin(function() {
-          uiChatboxInputBox.addClass('ui-chatbox-input-focus');
-          var box = $(this).parent().prev();
-          box.scrollTop(box.get(0).scrollHeight);
-        })
-        .focusout(function() {
-          uiChatboxInputBox.removeClass('ui-chatbox-input-focus');
+          $(this).val('');
+          return false;
+        }
+      })
+      .focusin(function() {
+        uiChatboxInputBox.addClass('ui-chatbox-input-focus');
+        var box = $(this).parent().prev();
+        box.scrollTop(box.get(0).scrollHeight);
+      })
+      .focusout(function() {
+        uiChatboxInputBox.removeClass('ui-chatbox-input-focus');
+      });
+
+      // disable selection
+      uiChatboxTitlebar.find('*').add(uiChatboxTitlebar).disableSelection();
+
+      // switch focus to input box when whatever clicked
+      uiChatboxContent.children().click(function() {
+          // click on any children, set focus on input box
+          self.uiChatboxInputBox.focus();
         });
 
-        // disable selection
-        uiChatboxTitlebar.find('*').add(uiChatboxTitlebar).disableSelection();
+      self._setWidth(self.options.width);
+      self._position(self.options.offset);
 
-        // switch focus to input box when whatever clicked
-        uiChatboxContent.children().click(function() {
-            // click on any children, set focus on input box
-            self.uiChatboxInputBox.focus();
-          });
+      self.options.boxManager.init(self);
 
-        self._setWidth(self.options.width);
-        self._position(self.options.offset);
-
-        self.options.boxManager.init(self);
-
-        if (!self.options.hidden) {
-          uiChatbox.show();
-        }
-      },
-      _setOption: function(option, value) {
-        if (value != null) {
-          switch (option) {
-            case "hidden":
-            if (value)
-              this.uiChatbox.hide();
-            else
-              this.uiChatbox.show();
-            break;
-            case "offset":
-            this._position(value);
-            break;
-            case "width":
-            this._setWidth(value);
-            break;
-          }
-        }
-        $.Widget.prototype._setOption.apply(this, arguments);
-      },
-      _setWidth: function(width) {
-        this.uiChatboxTitlebar.width(width + "px");
-        this.uiChatboxLog.width(width + "px");
-        this.uiChatboxInput.css("maxWidth", width + "px");
-        // padding:2, boarder:2, margin:5
-        this.uiChatboxInputBox.css("width", (width - 18) + "px");
-      },
-      _position: function(offset) {
-        this.uiChatbox.css("right", offset);
+      if (!self.options.hidden) {
+        uiChatbox.show();
       }
+    },
+    _setOption: function(option, value) {
+      if (value != null) {
+        switch (option) {
+          case "hidden":
+          if (value)
+            this.uiChatbox.hide();
+          else
+            this.uiChatbox.show();
+          break;
+          case "offset":
+          this._position(value);
+          break;
+          case "width":
+          this._setWidth(value);
+          break;
+        }
+      }
+      $.Widget.prototype._setOption.apply(this, arguments);
+    },
+    _setWidth: function(width) {
+      this.uiChatboxTitlebar.width(width + "px");
+      this.uiChatboxLog.width(width + "px");
+      this.uiChatboxInput.css("maxWidth", width + "px");
+      // padding:2, boarder:2, margin:5
+      this.uiChatboxInputBox.css("width", (width - 18) + "px");
+    },
+    _position: function(offset) {
+      this.uiChatbox.css("right", offset);
     }
-  );
+  } );
 }(jQuery));
