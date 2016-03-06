3ce71347d92f8c753139fdd5a5d81c760f41871b
Yanming Yang <ym.yang7@gmail.com>
2016-03-06 17:00:10 -0600

    Implemented tower damage with place holder damage value

--- backend/game_pieces/tower.py
+++ backend/game_pieces/tower.py
@@ -11,35 +11,35 @@ class Tower:
         self.cooldown = cooldown
         self.fire_range = fire_range
         self.id = id; #we need the towers to know where they are in the array of towers.
+        self.damage = 1; # PLACE HOLDER DAMAGE VALUE: TO BE REPLACED
 
         self.time_since_last_fire = 0
 
     def update(self, dt, living_creeps):
         self.time_since_last_fire += dt
-        fireOnCreeps = [];
+        myAttacks = [];
         if self.can_fire():
             x2, y2 = self.get_position()
             for creep in living_creeps:
                 x1, y1 = creep.get_position()
-                #x2, y2 = self.get_position()
-                print("CRREEEEEP ID")
-                print(creep.id)
                 if util.distance(x1, y1, x2, y2) <= self.fire_range:
                     self.fire(creep)
-                    fireOnCreeps.append(creep.id) #adds in all the fireable creeps to an array
-        return fireOnCreeps;
+                    myAttacks.append(creep.id) #adds in all the fireable creeps to an array
+        return myAttacks;
                     
     
 
 
     def can_fire(self):
         """True if the cooldown has warn off."""
-        #return self.time_since_last_fire < self.cooldown
-        return True
+        return self.time_since_last_fire <= self.cooldown
+        #return True
         
     def fire(self, target):
         """Fire at a target creep."""
         self.time_since_last_fire = 0
+        target.take_damage(self.damage);
+        
 
     def get_position(self):
         return self.loc[0], self.loc[1]
