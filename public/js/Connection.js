57d3091a49642b6968d2fcfe5dbe86254a9de269
zlailari <lailari.zeev@gmail.com>
2016-03-08 20:01:29 -0600

    Start parsing game update data

--- public/js/Connection.js
+++ public/js/Connection.js
@@ -10,14 +10,20 @@ ws.onclose = function() {
 };
 
 ws.onmessage = function(event) {
-    console.log('MESSAGE: ' + event.data);
-
     msg = safeParseJSON(event.data);
     if (msg && msg.hasOwnProperty('type')) {
+        console.log('MESSAGE: ' + JSON.stringify(msg, null, 2));
+
         if (msg.type == 'chat') {
             if (chatbox) {
                 chatbox.addMsg(msg.id, msg.msg);
             }
+        }
+        if (msg.type == 'gameUpdate') {
+            var playerState = msg['playerState'];
+            var creepLoc = msg['creepLoc'];
+            var attacksMade = msg['attacksMade'];
+            var creepProgress = msg['creepProgress'];
         }
     }
 };
