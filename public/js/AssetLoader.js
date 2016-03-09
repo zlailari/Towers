b245658110031e82ec05ab53883edad5021563da
zlailari <lailari.zeev@gmail.com>
2016-03-09 04:00:40 -0600

    Put all images in same place, made asset loader so we have all images loaded before the game can start

---
+++ public/js/AssetLoader.js
@@ -0,0 +1,32 @@
+// Loads all game images
+
+assetsLoaded = 0;
+assetsToLoad = 4;
+
+loadImages = function() {
+    // ADD ANY IMAGES TO LOAD HERE.
+    // CAN DEFINE IMAGE VAR IN YOUR FILE JUST REFERENCE IT HERE
+    // AN BE SURE TO ADD ONTO THE CONSTANT AT THE TOP WHEN YOU ADD ONE
+
+    creepPic = new Image();
+    creepPic.onload = function() {imageLoaded();};
+    creepPic.src = ('img/Demon3.png');
+
+    var towerImageNames = ["img/arrowHead.png",
+        "img/fire.png", "img/ice.png"];
+    for (var i = 0; i < towerImageNames.length; i++) {
+        towerImages[i] = new Image();
+        towerImages[i].onload = function() {imageLoaded();};
+        towerImages[i].src = towerImageNames[i];
+    }
+
+};
+
+imageLoaded = function() {
+    assetsLoaded++;
+
+    // check if all assets are loaded
+    if (assetsLoaded == assetsToLoad) {
+        fullyLoaded = true;
+    }
+};
