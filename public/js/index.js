770fa4b85179ca4c3534926384d8f6cd05913aa6
zlailari <lailari.zeev@gmail.com>
2016-04-23 23:25:19 -0500

    Basic introduction screen before game starts

---
+++ public/js/index.js
@@ -0,0 +1,30 @@
+// Handles game start and initial user interactions
+
+$(document).ready(function() {
+    $('#login-trigger').click(function(){
+        $(this).next('#login-content').slideToggle();
+        $(this).toggleClass('active');
+
+        if ($(this).hasClass('active')) {
+            $(this).find('span').html('&#x25B2;');
+        } else {
+            $(this).find('span').html('&#x25BC;');
+        }
+    });
+
+    $('.play-now').on('click', function() {
+        findGame();
+    });
+
+    $('.submit-login').on('click', function() {
+        console.log('login');
+    });
+
+    $('#signup').on('click', function() {
+        console.log('signup');
+    });
+});
+
+function findGame() {
+    $('.introduction').fadeOut(500);
+}
