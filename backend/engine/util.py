c8e9e374ae72e7c9eab16926e3f9f7836991c9a0
Yanming Yang <ym.yang7@gmail.com>
2016-04-24 23:20:01 -0500

    Added possible fix to diagonal movement problem.

--- backend/engine/util.py
+++ backend/engine/util.py
@@ -21,48 +21,76 @@ Y_COORD = 1
 
 # Position is current position and direction is where it's going.
 def edge(position, direction):
+
     if direction[X_COORD] == -1 and direction[Y_COORD] == 0: #going left
         if position[X_COORD] <= 0:  # currently at left edge of box
             return True, (1, position[Y_COORD] % 1) # move to right edge of next box
         return False, (-1,0)
+
     elif direction[X_COORD] == 1 and direction[Y_COORD] == 0: #going right
         if position[X_COORD] >= 1:# right edge of box
             return True, (0, position[Y_COORD] % 1) # move to left edge of next box
         return False, (1,0)
+
     elif direction[Y_COORD] == -1 and direction[X_COORD] == 0: #going down
         if position[Y_COORD] <= 0:  # bottom edge of box
             return True, (position[X_COORD] % 1, 1)  # move to top of next box
         return False, (0,-1)
+
     elif direction[Y_COORD] == 1 and direction[X_COORD] == 0: #going up
         if position[Y_COORD] >= 1:  # top edge of box
             return True, (position[X_COORD] % 1, 0) #move to bottom of next box
         return False, (0,1)
+
     elif direction[X_COORD] == -1 and direction[Y_COORD] == -1: # going top left
-        if position[X_COORD] <= 0: #current position is at left edge of box
-            if position[Y_COORD] <=0: #current position is at bottom left
-                return True, (1,1) #move to top right edge of next box
-            return False, (0,-1)
-        return False,(-1,0)
+        if at_left(position): #current position is at left edge of box
+            if at_top(position): #current position is at top left
+                return True, (1,1) #move to bottom right edge of next box
+            return False, (0,-1) #at left edge but not at top, move up
+        if at_top(position):
+            return False,(-1,0) #move left
+        return False,(-1,-1) #Move top left
+
     elif direction[X_COORD] == 1 and direction[Y_COORD] == 1: # going bottom right
-        if position[X_COORD] >= 1: #current position is at right edge of box
-            if position[Y_COORD] >=1: #current position is at top right
-                return True, (0,0) #move to bottom left edge of next box
+        if at_right(position): #current position is at right edge of box
+            if at_bottom(position): #current position is at bottom right
+                return True, (0,0) #move to top left edge of next box
             return False,(0,1)
-        return False,(1,0)
+        if at_bottom(position):
+            return False,(1,0)
+        return False, (1,1) #Move bottom right
+
     elif direction[X_COORD] == -1 and direction[Y_COORD] == 1: # going bottom left
-        if position[X_COORD] <= 0: #current position is at left edge of box
-            if position[Y_COORD] >=1: #current position is at top left
+        if at_left(position): #current position is at left edge of box
+            if at_bottom(position): #current position is at bottom left
                 return True, (1,0) #move to bottom right edge of next box
             return False,(0,1)
-        return False, (-1,0)
+        if at_bottom(position):
+            return False, (-1,0)
+        return False, (-1,1)
+
     elif direction[X_COORD] == 1 and direction[Y_COORD] == -1: # going top right
-        if position[X_COORD] >= 1: #current position is at right edge of box
-            if position[Y_COORD] <=0: #current position is at bottom right
+        if at_right(position): #current position is at right edge of box
+            if at_top(position): #current position is at top right
                 return True, (0,1) #move to top left edge of next box
             return False, (0,-1)
-        return False, (1,0)
+        if at_top(position):
+            return False, (1,0)
+        return False, (1,-1)
     else:
         return False, -1
+
+def at_top(coord):
+    return coord[Y_COORD]<=0
+
+def at_bottom(coord):
+    return coord[Y_COORD]>=0
+
+def at_left(coord):
+    return coord[X_COORD]<=0
+
+def at_right(coord):
+    return coord[Y_COORD]>=0
 
 
 def utf(i):
