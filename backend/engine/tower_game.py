dc6d845ac7f2259b983d4d7a6acd10945aaf766e
Yanming Yang <ym.yang7@gmail.com>
2016-04-27 00:04:01 -0500

    Creeps now cost their bounty when spawning to other players. Note that the if statement that prevents you from sending to yourself is commented out.

--- backend/engine/tower_game.py
+++ backend/engine/tower_game.py
@@ -15,6 +15,10 @@ WORLD_WIDTH = 16
 WORLD_HEIGHT = 12
 
 INFO_ID = 'game engine'
+#if type == "Default": return Creep((0,0),"default",.05,100,id, 15)
+#if type == "Fast": return Creep((0,0),"strong",.1,50,id,30)
+#if type == "Slow": return Creep((0,0),"weak",.005,1000,id,100)
+creep_costs = {'Default':15, 'Fast':30, 'Slow':100}
 
 
 class GameRunner:
@@ -141,11 +145,16 @@ class GameRunner:
         elif msg['type'] == MSG.creep_request.name:
             player_id = msg['player_id']
             creep_type = msg['msg']['creepID']
-            for player in self.player_states:
-                #if player != player_id:
-                state = self.player_states[player]
-                state.spawn_creep(creep_type)
-                self.player_states[player_id].spawned_creeps +=1
+
+            if((self.player_states[player_id].gold)>= creep_costs[creep_type]):
+                for player in self.player_states:
+                    #if player != player_id:
+
+
+                    state = self.player_states[player]
+                    state.spawn_creep(creep_type)
+                    self.player_states[player_id].spawned_creeps +=1
+                    self.player_states[player_id].gold=self.player_states[player_id].gold-creep_costs[creep_type]
 
 
     def game_loop(self, dt):
